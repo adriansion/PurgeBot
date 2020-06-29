c42603b92c38b28cf64e55bf5561027592e65ff7
Adrian Sion <adriandavidsion@gmail.com>
2020-06-29 02:00:04 -0400

    Overhauled deletion progress logging

--- src/main/java/Purger.java
+++ src/main/java/Purger.java
@@ -4,11 +4,15 @@ import org.javacord.api.entity.message.MessageSet;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.javacord.api.exception.UnknownMessageException;
 
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Purger {
 
@@ -45,6 +49,15 @@ public class Purger {
         ArrayList<Message> userMessages = new ArrayList<>();
         Stack<ArrayList<Message>> deletionBatches = new Stack<>();
 
+        AtomicInteger userMessageCount = new AtomicInteger(0);
+        AtomicInteger deletedMessageCount = new AtomicInteger(0);
+        AtomicInteger deletionPercentFinished = new AtomicInteger(0);
+        AtomicInteger deletionProgressLogsSent = new AtomicInteger(0);
+        AtomicIntegerArray loggerProgressNotifyThresholdsAtomic;
+        AtomicBoolean allDeletionsSuccessful;
+        boolean logCompletionPercentagePerBatch;
+
+
         try {
 
             // Collects all messages sent to channel since user joined server.
@@ -57,6 +70,7 @@ public class Purger {
             for (Message m : allMessages) {
                 if (m.getAuthor().getDiscriminatedName().equals(user)) {
                     userMessages.add(m);
+                    userMessageCount.incrementAndGet();
                 }
             }
 
@@ -64,19 +78,31 @@ public class Purger {
             e.printStackTrace();
         }
 
-        logger.info(userMessages.size() + " messages will be deleted. [Channel: " + c.getName() + "]");
+        logger.info(userMessageCount.toString() + " messages will be deleted. [Channel: " + c.getName() + "]");
 
 
         // Distributes user messages to deletion batch arrays.
+        int maxArraySize = 100; // 100
         deletionBatches.push(new ArrayList<>());
         for (Message m : userMessages) {
-            if (deletionBatches.peek().size() >= 100) {
+            if (deletionBatches.peek().size() >= maxArraySize) {
                 deletionBatches.push(new ArrayList<>());
             }
             deletionBatches.peek().add(m);
         }
 
-        AtomicBoolean allDeletionsSuccessful = new AtomicBoolean(true);
+        int maxProgressLogCount = 14;
+        logCompletionPercentagePerBatch = (userMessageCount.intValue() <= maxArraySize * maxProgressLogCount);
+        int[] loggerProgressNotifyThresholds = new int[maxProgressLogCount];
+        if (!logCompletionPercentagePerBatch) {
+            int thresholdMultiplier = (int) ((float) userMessageCount.intValue() / (float) maxProgressLogCount);
+            for (int i = 1; i <= maxProgressLogCount; i++) {
+                loggerProgressNotifyThresholds[i - 1] = thresholdMultiplier * i;
+            }
+        }
+
+        loggerProgressNotifyThresholdsAtomic = new AtomicIntegerArray(loggerProgressNotifyThresholds);
+        allDeletionsSuccessful = new AtomicBoolean(true);
         HashMap<CompletableFuture<Void>, Integer> fm = new HashMap<>();
         Runnable task = () -> {
             for (ArrayList<Message> a : deletionBatches) {
@@ -85,10 +111,24 @@ public class Purger {
 
             fm.forEach((f, i) -> {
                 try {
-                    logger.info("Deleting batch of " + i + " message(s)...");
+                    deletedMessageCount.set(deletedMessageCount.intValue() + i);
+
+                    // Periodically log percentage completion.
+                    if (logCompletionPercentagePerBatch ||
+                            deletedMessageCount.intValue() >= (loggerProgressNotifyThresholdsAtomic
+                                    .get(deletionProgressLogsSent.intValue())) ||
+                            deletedMessageCount.intValue() == 0) {
+                        deletionProgressLogsSent.incrementAndGet();
+                        deletionPercentFinished.set((int) ((((float) deletedMessageCount.intValue()) / ((float) userMessageCount.intValue())) * 100));
+                        logger.info("Deletion " + deletionPercentFinished.toString() + "% complete. [Channel: " + c.getName() + "]");
+                    }
+
                     f.get();
                 } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
+                    if (!e.getMessage().equalsIgnoreCase("Unknown Message")) {
+                        e.printStackTrace();
+                    }
+
                 }
                 if (!f.isDone()) {
                     allDeletionsSuccessful.set(false);
